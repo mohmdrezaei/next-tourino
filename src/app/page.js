c0e0c13583f3396883bf524550fcec61ed4532bc
mohmdrezaei <mhwmdrz9@gmail.com>
2024-12-07 00:48:25 +0330

    Getting the list data of all tours on the server side

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
 import Image from "next/image";
+
 import { FaPhoneAlt } from "react-icons/fa";
 import { FaQuestion } from "react-icons/fa";
-export default function Home() {
+async function Home() {
+ const data =  await fetch("http://localhost:6500/tour",{ cache: 'no-store' })
+    .then((res) => res.json())
+    .then((data) => data);
   return (
     <div className=" ">
       <Image
@@ -9,6 +13,7 @@ export default function Home() {
         src="/images/landing.svg"
         width="1440"
         height="350"
+        alt="banner"
       />
 
       <div className="text-center mt-5">
@@ -22,46 +27,49 @@ export default function Home() {
 
       <div className="w-[1200px] mx-auto">
         <h3 className="font-normal text-[32px] tracking-wide">همه تور ها</h3>
-        <div className="grid grid-cols-4 mt-3">
-          <div class="w-[278px] bg-white border border-[#0000001F] drop-shadow-sm rounded-[10px] ">
-            <a href="#">
-              <Image
-                class="rounded-t-lg"
-                src="/images/arbil.svg"
-                alt=""
-                width="278"
-                height="159"
-              />
-            </a>
-            <div class="px-5 pt-3 border border-b-[#D9D9D9]">
-              <a href="#">
-                <h5 class="mb-2 text-[27px] font-normal tracking-tight text-gray-900 dark:text-white">
-                  اربیل
-                </h5>
-              </a>
-              <p class="mb-3 font-normal text-[#282828B2] dark:text-gray-400">
-                مهرماه . 3 روزه- پرواز - هتل3 س...
-              </p>
-            </div>
-            <div className="flex justify-between px-3 py-2 text-base ">
-              <button className="bg-[#28A745] text-white rounded px-5">
-                رزرو
-              </button>
-              <p className="text-[16px] px-1">
-                <span className="text-[#009eca] px-1">17500000</span> تومان
-              </p>
-            </div>
-          </div>
+        <div className="grid grid-cols-4 mt-3 gap-8">
+         {data.map(tour =>(
+           <div key={tour.id} class="w-[278px] bg-white border border-[#0000001F] drop-shadow-sm rounded-[10px] ">
+           <a href="#">
+             <img
+               class="rounded-t-lg"
+               src={tour.image}
+               alt={tour.title}
+               width="278"
+               height="159"
+              
+             />
+           </a>
+           <div class="px-5 pt-3 border border-b-[#D9D9D9]">
+             <a href="#">
+               <h5 class="mb-2 text-[27px] font-normal tracking-tight text-gray-900 dark:text-white">
+               {tour.title}
+               </h5>
+             </a>
+             <p class="mb-3 font-normal text-[#282828B2] dark:text-gray-400">
+             {tour.options}.
+             </p>
+           </div>
+           <div className="flex justify-between px-3 py-2 text-base ">
+             <button className="bg-[#28A745] text-white rounded px-5">
+               رزرو
+             </button>
+             <p className="text-[16px] px-1">
+               <span className="text-[#009eca] px-1">{tour.price}</span> تومان
+             </p>
+           </div>
+         </div>
+         ))}
         </div>
 
         <div className="flex  border border-[#00000040] h-[251px] mt-20 rounded-[10px] ">
           <div className="bg-[#28A745] rounded-[10px] w-[870px] text-white relative  ">
             <div className="m-12">
               <h1 className="text-5xl font-semibold ">
-                خرید تلفنی از{" "}
+                خرید تلفنی از
                 <sapn className="text-5xl font-semibold text-[#10411b]">
                   تورینو
-                </sapn>{" "}
+                </sapn>
               </h1>
               <p className="text-[32px] font-light mx-3 mt-4">
                 به هر کجا که میخواهید
@@ -94,10 +102,14 @@ export default function Home() {
                 <FaQuestion />
               </div>
               <h3 className="text-[40px] font-normal">
-              چرا <span className="text-[40px] font-normal text-[#28a745]">تورینو</span> ؟
-            </h3>
+                چرا
+                <span className="text-[40px] font-normal text-[#28a745]">
+                  تورینو
+                </span>
+                ؟
+              </h3>
             </div>
-           
+
             <p className="font-normal mt-5 text-2xl">تور طبیعت گردی و تاریخی</p>
             <p className="w-[517px] mt-5 text-xl text-[#282828] leading-10 ">
               اگر دوست داشته باشید که یک جاذبه طبیعی را از نزدیک ببینید و در دل
@@ -112,3 +124,5 @@ export default function Home() {
     </div>
   );
 }
+
+export default Home;
