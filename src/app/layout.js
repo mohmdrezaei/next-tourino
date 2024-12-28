96d4a258c752ce5d5054911b89a3b6a2e6743027
mohmdrezaei <mhwmdrz9@gmail.com>
2024-12-28 18:26:19 +0330

    Add favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <link rel="icon" href="/images/favicon.svg" sizes="any" />Z
       <body className={yekan.className}>
         <TanstackQueryProvider>
         <MuiProvider>
