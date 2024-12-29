8bb8abda1f3d80432169a0f33306bc653151573d
mohmdrezaei <mhwmdrz9@gmail.com>
2024-12-30 01:36:01 +0330

    Handling errors when booking a tour if the user is not logged in

--- src/components/ui/widgets/ReserveButton.js
+++ src/components/ui/widgets/ReserveButton.js
@@ -1,15 +1,21 @@
 "use client"
 
 import { useAddToBasket } from "@/services/mutations";
+import { useGetUser } from "@/services/queries";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 
 function ReserveButton({id}) {
+   const {data} = useGetUser()
     const { isPending, mutate } = useAddToBasket();
   const router = useRouter()
   const addHandler = ( ) => {
    
     if(isPending) return
+    if(!data) {
+      toast.error("برای رزرو تور ابتدا وارد سایت شوید!")
+      return
+    }
     mutate(id, { onSuccess: (data) => {
       router.push("/basket")
       toast.success(data.data.message)
